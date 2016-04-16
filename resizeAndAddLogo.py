12d4bd049e2031ef01dcafda33b9b19b65ea1d5c
Brandon Willett <bwillett@mail.valenciacollege.edu>
2016-04-16 09:26:04 -0400

    #3

--- resizeAndAddLogo.py
+++ resizeAndAddLogo.py
@@ -10,28 +10,28 @@ logoWidth, logoHeight = logoIm.size
 os.makedirs('withLogo', exist_ok=True)
 
 for filename in os.listdir('.'):
-  if not (filename.endswith('.png') or filename.endswith('.jpg')) \
-    or filename == LOGO_FILENAME:
-      continue
+    if not (filename.endswith('.png') or filename.endswith('.jpg')) \
+       or filename == LOGO_FILENAME:
+        continue
 
-  im = Image.open(filename)
-  width, height = im.size
+    im = Image.open(filename)
+    width, height = im.size
   
-  if width > SQUARE_FIT_SIZE and height > SQUARE_FIT_SIZE:
-    if width > height:
-      height = int((SQUARE_FIT_SIZE / width) * height)
-      width = SQUARE_FIT_SIZE
-    else:
+    if width > SQUARE_FIT_SIZE and height > SQUARE_FIT_SIZE:
+      if width > height:
+        height = int((SQUARE_FIT_SIZE / width) * height)
+        width = SQUARE_FIT_SIZE
+      else:
         width = int((SQUARE_FIT_SIZE / height) * width)
         height = SQUARE_FIT_SIZE
         
-        print('Resizing %s...' % (filename))
-        im = im.resize((width, height))
+    print('Resizing %s...' % (filename))
+    im = im.resize((width, height))
 
-        print('Adding logo to %s...' % (filename))
-        im.paste(logoIm, (width - logoWidth, height - logoHeight), logoIm)
+    print('Adding logo to %s...' % (filename))
+    im.paste(logoIm, (width - logoWidth, height - logoHeight), logoIm)
 
-        im.save(os.path.join('withLogo', filename))
+    im.save(os.path.join('withLogo', filename))
 
 #TODO: Loop over all files in the working directory
 #TODO: Check if file image needs to be
